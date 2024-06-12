f6a5847317b3501c585aec8eaefe3bb0f8e1b580
lilyoh <lilyoh628@gmail.com>
2024-06-12 15:30:14 +1000

    FEAT: infinite carousel finished

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,24 +1,35 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 const banners = ['banner1', 'banner2', 'banner3', 'banner4', 'banner5', 'banner6', 'banner7'];
+const duplicatedBanners = [banners[banners.length - 1], ...banners, banners[0]];
 
 const Carousel = () => {
-	const [imageIndex, setImageIndex] = useState<number>(0);
+	const [currentBannerIndex, setCurrentBannerIndex] = useState<number>(1);
 	const [isBannerHovered, setIsBannerHovered] = useState<boolean>(false);
+	const durationDiv = useRef<HTMLDivElement>(null);
 
 	function showPreviousBanner() {
-		setImageIndex((index) => {
+		if (currentBannerIndex === duplicatedBanners.length - 2) {
+			if (!durationDiv.current) return;
+			durationDiv.current.style.transitionDuration = '';
+		}
+		setCurrentBannerIndex((index) => {
 			if (index === 0) return banners.length - 1;
 			return index - 1;
 		});
 	}
 
 	function showNextBanner() {
-		setImageIndex((index) => {
-			if (index === banners.length - 1) return 0;
+		if (currentBannerIndex === 1) {
+			if (!durationDiv.current) return;
+			durationDiv.current.style.transitionDuration = '';
+		}
+
+		setCurrentBannerIndex((index) => {
+			if (index === duplicatedBanners.length - 1) return 2;
 			return index + 1;
 		});
 	}
@@ -31,22 +42,40 @@ const Carousel = () => {
 
 	return (
 		<div
-			className="slider-item-container relative flex overflow-hidden max-w-[1440px] m-auto"
+			className="relative max-w-[1440px] m-auto overflow-hidden"
 			onMouseEnter={() => setIsBannerHovered(true)}
 			onMouseLeave={() => setIsBannerHovered(false)}
 		>
-			{banners.map((slide) => (
-				<Image
-					key={slide}
-					src={`/images/${slide}.jpeg`}
-					alt={`${slide}`}
-					width={1920}
-					height={600}
-					style={{ transform: `translateX(${-100 * imageIndex}%)` }}
-					className="shrink-0 w-full h-full transition-transform duration-200 ease-in-out"
-				/>
-			))}
-			<button className={`absolute top-[50%] left-[15%] ${isBannerHovered ? 'block' : 'hidden'}`}>
+			<div
+				className="flex transition-transform duration-200 ease-in-out"
+				style={{ transform: `translateX(${-100 * currentBannerIndex}%)` }}
+				ref={durationDiv}
+				onTransitionEnd={() => {
+					if (currentBannerIndex === duplicatedBanners.length - 1) {
+						if (!durationDiv.current) return;
+						durationDiv.current.style.transitionDuration = '0ms';
+						setCurrentBannerIndex(1);
+					}
+
+					if (currentBannerIndex === 0) {
+						if (!durationDiv.current) return;
+						durationDiv.current.style.transitionDuration = '0ms';
+						setCurrentBannerIndex(duplicatedBanners.length - 2);
+					}
+				}}
+			>
+				{duplicatedBanners.map((banner) => (
+					<Image
+						key={banner}
+						src={`/images/${banner}.jpeg`}
+						alt={`${banner}`}
+						width={1920}
+						height={600}
+						className="shrink-0 w-full h-full"
+					/>
+				))}
+			</div>
+			<button className={`absolute top-[50%] -translate-y-1/2 left-[15%] ${isBannerHovered ? 'block' : 'hidden'}`}>
 				<Image
 					src="/images/prev-button.png"
 					alt="previous button"
@@ -55,7 +84,7 @@ const Carousel = () => {
 					onClick={showPreviousBanner}
 				/>
 			</button>
-			<button className={`absolute top-[50%] right-[15%] ${isBannerHovered ? 'block' : 'hidden'}`}>
+			<button className={`absolute top-[50%] -translate-y-1/2 right-[15%] ${isBannerHovered ? 'block' : 'hidden'}`}>
 				<Image
 					src="/images/next-button.png"
 					alt="next button"
