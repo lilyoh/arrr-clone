7712d885e99410914818ec236e98a6d159302195
lilyoh <lilyoh628@gmail.com>
2024-06-13 21:04:23 +1000

    CHORE: install packages and setting test environment

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		exclude: [...configDefaults.exclude, '**/tests/**'],
+		setupFiles: ['./vitest-setup.ts'],
+		environment: 'jsdom',
+	},
+});
